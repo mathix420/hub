c8cd2b63fd270f63ddeb96878eadc90047d45168
Mathix 420 <gisingerarnaud@gmail.com>
2019-07-06 18:41:35 +0200

    performing some tests

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ from util import *
 logger = logging.getLogger()
 
 def send_file(socket, address, path):
-        print('START FILE')
+        print('STARTING FILE SHARING')
         with open(path, 'rb') as fp:
                 bytes = gzip.compress(fp.read())
         socket.sendto(b'#!#prefix#!#', address)
@@ -22,35 +22,48 @@ def send_file(socket, address, path):
         else:
                 print('error cannot send file')
 
-def main(host='e3r1p11.42.fr', port=9999, sender=False):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.sendto(b'I want address', (host, port))
-        while True:
-                data, addr = sock.recvfrom(1024)
-                print('client received: {} {}'.format(addr, data))
-                # sock.sendto(b'Hello', addr)
-                if data[:9] == b'#!#prefix':
-                        print('IN')
-                        sock.sendto(b'#!#can_get_file#!#', addr)
-                        file_bytes = b''
+def recieve_file_from_peer(peer_addr, sock):
+        data, addr = sock.recvfrom(2048)
+        if addr != peer_addr:
+                return False
+        with open('/tmp/recv.zip', 'ab+') as fp:
+                while data != b'#!#end_of_file#!#':
+                        fp.write(data)
                         data, addr = sock.recvfrom(2048)
-                        with open('/tmp/recv.zip', 'ab+') as fp:
-                                while data != b'#!#end_of_file#!#':
-                                        file_bytes += data
-                                        data, addr = sock.recvfrom(2048)
-                                        fp.write(file_bytes)
-                        # with open('/tmp/recv.zip', 'ab+') as fp:
-                        #         with open('/tmp/recv.zip', 'wb+') as fw:
-                        #                 fw.write(gzip.decompress(fp.read()))
-                        print("FILE WRITED CORRECTLY (I BET)")
+        with open('/tmp/recv.zip', 'rb') as fp:
+                data = gzip.decompress(fp.read())
+        with open('/tmp/recv.zip', 'wb') as fp:
+                fp.write(data)
+        return True
+
+
+def main(server_host='127.0.0.1', server_port=9999, sender=False):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        # There I ask the server for the other person address
+        sock.sendto(b'I want address', (server_host, server_port))
+
+        # Then I wait for a response
+        data, peer_addr = sock.recvfrom(1024)
+        print('peer infos recieved from server: {} {}'.format(peer_addr, data))
+
+        # Now we can start listening from peer
+        while True:
+                if data[:9] == b'#!#prefix':
+                        print('GOT FILE PREFIX')
+                        sock.sendto(b'#!#can_get_file#!#', peer_addr)
+                        recieve_file_from_peer(peer_addr, sock)
+                        exit()
                 else:
                         addr = msg_to_addr(data)
                         if sender:
                                 print('OK')
                                 send_file(sock, addr, '/tmp/send.zip')
-                        print('NO')
+                                exit()
+                        else:
+                                print('NO')
+                data, peer_addr = sock.recvfrom(1024)
 
-                print('client received: {} {}'.format(addr, data))
 
 if __name__ == '__main__':
         logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
